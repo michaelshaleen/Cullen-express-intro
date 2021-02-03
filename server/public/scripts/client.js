1aaa713d31449a7f23007ab8577d6d6b2829efe5
Michael Shaleen <shaleenmichael@protonmail.com>
2021-02-03 16:11:21 -0600

    fixed some issues

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,18 +5,35 @@ console.log('in client.js');
 
 function onReady() {
   console.log('ready');
+  fetchQuotes();
 
   //grab data from server
   // get all the quotes
+}
+
+console.log('so much to do');
+
+//.then is new shit
+
+// ajax need an object
+// Asynchronous Javascript and XML = ajax
+//tool for making network requests
+// http network requests
+//FROM  client-side js code
+//ajax is function that comes w/ jq
+//take array of quotes
+// loop and append to DOM boi
+
+function fetchQuotes() {
   let ajaxOptions = {
     url: '/allTheQuotes',
     method: 'GET',
   };
+
   $.ajax(ajaxOptions)
     //promise to call me back later
-    .then(function (response) {
-      console.log('response', response);
-
+    .then(function (quoteList) {
+      console.log('response', quoteList);
 
       for (let quote of quoteList) {
         console.log(quote);
@@ -29,25 +46,9 @@ function onReady() {
         </li>
         `);
       }
+    })
+
+    .catch(function () {
+      alert('whoops');
     });
-
-   
-      
-    }
-    .catch(function() {
-      alert("whoops")
-
-    });
-
-  console.log('so much to do');
-
-  //.then is new shit
-
-  // ajax need an object
-  // Asynchronous Javascript and XML = ajax
-  //tool for making network requests
-  // http network requests
-  //FROM  client-side js code
-  //ajax is function that comes w/ jq
-  //take array of quotes
-  // loop and append to DOM boi
+}
